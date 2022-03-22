f4a244575cdace2ae025693e50d5aaed7c53dcc7
Andrew LeCody <andrew@beyondweb.net>
2022-03-21 23:00:55 -0500

    Move SECRET_KEY to .env so I stop getting alerts

--- storage_management/settings.py
+++ storage_management/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x#baja6k7m!06ttw&x#1&_(=1^)3zjrh+u!mmf3r_zbjslatc@"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
